875433375a95596ff85d400c84b6457c5c2d3d7d
Sidhi Jain <sidhijain482@gmail.com>
2024-07-11 18:55:40 +0530

    Sample data sending from backend to frontend

--- dhana-sarathi-backend/index.js
+++ dhana-sarathi-backend/index.js
@@ -1,12 +1,26 @@
-import http from "http";
+import express from "express";
+import cors from "cors";
+const app = express();
+const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Hello World\n");
+app.use(express.json());
+app.use(cors());
+
+// Define a simple route
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
 
-const PORT = 3000;
-server.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}/`);
+// Define a route to get data
+app.get('/api/data', (req, res) => {
+  const data = [
+    { id: 1, name: 'Sidhi' },
+    { id: 2, name: 'Anish' },
+    { id: 3, name: 'Diwakar' },
+  ];
+  res.json(data);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
 });
--- dhana-sarathi-frontend/src/app/app.component.ts
+++ dhana-sarathi-frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DataTransferService } from './data-transfer.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'dhana-sarathi-frontend';
+  constructor(private dataService :DataTransferService){}
+  data:any[]=[];
+  ngOnInit(): void {
+    this.dataService.getData().subscribe((data) => {
+      this.data = data;
+    });
+  }
 }
